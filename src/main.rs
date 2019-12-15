89fe333c08f81997093f3592a4a106b385f18b39
Fabio Pungg <fabiopungg@gmail.com>
2019-12-15 18:57:08 +0100

    part 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::convert::TryFrom;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 #[derive(Debug, PartialEq)]
 enum Opcode {
-    add,
-    mult,
-    stop,
+    Add,
+    Mult,
+    Stop,
+    Err,
 }
 
 #[derive(Debug)]
@@ -20,9 +22,25 @@ fn parse_program(input: &str) -> Vec<u8> {
     input.split(',').map(|s| s.parse::<u8>().unwrap()).collect()
 }
 
-impl IntCode {
-    fn verify_len(&self, len: usize) -> bool {
-        len > self.in1 as usize && len > self.in2 as usize && len > self.out as usize
+impl From<u32> for Opcode {
+    fn from(x: u32) -> Self {
+        match x {
+            1 => Opcode::Add,
+            2 => Opcode::Mult,
+            99 => Opcode::Stop,
+            _ => Opcode::Err,
+        }
+    }
+}
+
+impl From<&[u32]> for IntCode {
+    fn from(x: &[u32]) -> Self {
+        IntCode {
+            opcode: Opcode::from(x[0]),
+            in1: x[1] as usize,
+            in2: x[2] as usize,
+            out: x[3] as usize,
+        }
     }
 }
 
@@ -34,86 +52,104 @@ fn main() {
         let line = line.unwrap();
         buf.push_str(&line);
     }
-    
     // let mut buf = String::from("1,9,10,3,2,3,11,0,99,30,40,50");
-    let program_data = parse_program(&buf);
-    let mut ga = GravityAssist::new(&program_data);
-    let mut program_data = program_data;
+    let mut program_data = parse_program(&buf);
 
+    let mut ga = GravityAssist::new(&program_data);
+    println!("{:?}", ga.state);
     while ga.exec_step() != None {
-        println!("{}", ga.current_instruction());
+        println!("{}:{}", ga.pc, ga.current_instruction());
     }
 
     print!("{:?}", ga.state);
 }
 
 struct GravityAssist {
-    data: Vec<u8>,
     state: Vec<u32>,
-    instructions: Vec<IntCode>,
     pc: usize,
 }
 
 impl GravityAssist {
     pub fn current_instruction(&self) -> String {
-        let x = String::from(format!("{:?}", &self.instructions[self.pc]));
+        let x = String::from(format!("{:?}", self.parse_current_instruction()));
         x
     }
 
-    pub fn exec_step(&mut self) -> Option<Opcode> {
-        let inst = &self.instructions[self.pc];
+    pub fn set_1202_error_state(&mut self) {
+        self.state[1] = 12;
+        self.state[2] = 2;
+    }
+
+    pub fn exec_step(&mut self) -> Option<()> {
+        // let inst = &self.instructions[self.pc];
+        let inst = self.parse_current_instruction().unwrap();
         let mut ret = match inst.opcode {
-            Opcode::add => {
+            Opcode::Add => {
                 self.state[inst.out] = self.state[inst.in1] + self.state[inst.in2];
-                Some(Opcode::add)
+                Some(())
             }
-            Opcode::mult => {
+            Opcode::Mult => {
                 self.state[inst.out] = self.state[inst.in1] * self.state[inst.in2];
-                Some(Opcode::mult)
+                Some(())
             }
-            Opcode::stop => {
-                println!("ERROR");
+            Opcode::Stop => {
+                println!("Stop Code");
+                None
+            }
+            Opcode::Err => {
+                println!("Illegal Opcode {}", self.state[self.pc]);
                 None
             }
         };
-        self.pc += 1;
-        if self.pc >= self.instructions.len() {
-            self.pc = self.instructions.len() - 1;
+        self.pc += 4;
+        if self.pc >= self.state.len() - 3 {
+            self.pc = self.state.len() - 3;
             ret = None;
         }
         ret
     }
 
-    fn parse_instructions(data: &Vec<u8>) -> Option<Vec<IntCode>> {
-        let index = data.iter().position(|x| *x == 99).unwrap();
-        let (dat_short, _) = data.split_at(index);
+    fn parse_current_instruction(&self) -> Option<IntCode> {
+        if self.state.len() - self.pc < 3 {
+            return None;
+        }
+        let slice = &self.state[self.pc..self.pc + 4];
 
-        Some(
-            dat_short
-                .chunks_exact(4)
-                .map(|x| IntCode {
-                    opcode: match x[0] {
-                        1 => Some(Opcode::add),
-                        2 => Some(Opcode::mult),
-                        99 => Some(Opcode::stop),
-                        _ => None,
-                    }
-                    .unwrap(),
-                    in1: x[1] as usize,
-                    in2: x[2] as usize,
-                    out: x[3] as usize,
-                })
-                .collect(),
-        )
+        IntCode::try_from(slice);
+        match IntCode::try_from(slice) {
+            Ok(x) => Some(x),
+            Err(_) => None,
+        }
     }
+
+    // fn parse_instructions(data: &Vec<u8>) -> Option<Vec<IntCode>> {
+    //     let index = data.iter().position(|x| *x == 99).expect("Opcode Stop not found!");
+    //     let (dat_short, _) = data.split_at(index);
+
+    //     let instructions = dat_short
+    //         .chunks_exact(4)
+    //         .map(|x| IntCode {
+    //             opcode: match x[0] {
+    //                 1 => Some(Opcode::Add),
+    //                 2 => Some(Opcode::Mult),
+    //                 99 => Some(Opcode::Stop),
+    //                 _ => None,
+    //             }
+    //             .expect("illegal Opcode"),
+    //             in1: x[1] as usize,
+    //             in2: x[2] as usize,
+    //             out: x[3] as usize,
+    //         })
+    //         .collect();
+    //     Some(instructions)
+    // }
 
     pub fn new(data: &Vec<u8>) -> Self {
         // let test = data.iter();
         let state: Vec<u32> = data.iter().map(|x| *x as u32).collect();
         GravityAssist {
-            data: data.to_owned(),
             state: state.to_owned(),
-            instructions: GravityAssist::parse_instructions(data).unwrap(),
+            // instructions: GravityAssist::parse_instructions(data).unwrap(),
             pc: 0,
         }
     }
@@ -123,6 +159,6 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let test_input = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+        // let test_input = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
     }
 }
