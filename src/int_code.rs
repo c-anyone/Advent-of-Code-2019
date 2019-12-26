e745ffe426ed511eedf467767db1fb7598f007d8
Fabio Pungg <fabiopungg@gmail.com>
2019-12-26 17:04:48 +0100

    almost ready to test the old input program again

--- src/int_code.rs
+++ src/int_code.rs
@@ -1,164 +1,288 @@
-use num::PrimInt;
-use std::convert::TryFrom;
+// #[derive(Debug, PartialEq)]
+// pub enum Parameter {
+//     Positional,
+//     Immediate,
+// }
 
-pub fn parse_program(input: &str) -> Result<Vec<i32>, std::num::ParseIntError> {
-    input.split(',').map(|s| s.parse()).collect()
-}
+// #[derive(Debug)]
+// pub struct IntCode {
+//     opcode: Opcode,
+//     in1: usize,
+//     in2: usize,
+//     out: usize,
+// }
 
-#[derive(Debug, PartialEq)]
-pub enum Parameter {
-    Positional,
-    Immediate,
-}
+// impl From<&[i32]> for IntCode {
+//     fn from(x: &[i32]) -> Self {
+//         IntCode {
+//             opcode: Opcode::from(x[0]),
+//             in1: x[1] as usize,
+//             in2: x[2] as usize,
+//             out: x[3] as usize,
+//         }
+//     }
+// }
 
-#[derive(Debug)]
-pub struct IntCode {
-    opcode: Opcode,
-    in1: usize,
-    in2: usize,
-    out: usize,
-}
+// #[derive(Debug, PartialEq)]
+// pub enum Opcode {
+//     Add,
+//     Mult,
+//     Stop,
+//     Input,
+//     Output,
+//     Err,
+// }
 
-impl From<&[i32]> for IntCode {
-    fn from(x: &[i32]) -> Self {
-        IntCode {
-            opcode: Opcode::from(x[0]),
-            in1: x[1] as usize,
-            in2: x[2] as usize,
-            out: x[3] as usize,
-        }
-    }
-}
+// impl From<i32> for Opcode {
+//     fn from(x: i32) -> Self {
+//         let op = if x > 99 { x % 100 } else { x };
+//         let param = x - op;
+//         match op {
+//             1 => Opcode::Add,
+//             2 => Opcode::Mult,
+//             3 => Opcode::Input,
+//             4 => Opcode::Output,
+//             99 => Opcode::Stop,
+//             _ => Opcode::Err,
+//         }
+//     }
+// }
 
-#[derive(Debug, PartialEq)]
-pub enum Opcode {
-    Add,
-    Mult,
-    Stop,
-    Input,
-    Output,
-    Err,
-}
+// impl Opcode {
+//     pub fn len(&self) -> usize {
+//         match self {
+//             Opcode::Add => 4,
+//             Opcode::Mult => 4,
+//             Opcode::Input => 2,
+//             Opcode::Output => 2,
+//             Opcode::Stop => 1,
+//             Opcode::Err => 0,
+//         }
+//     }
+// }
 
-impl From<i32> for Opcode {
-    fn from(x: i32) -> Self {
-        let op = if x > 99 { x % 100 } else { x };
-        let param = x - op;
-        match op {
-            1 => Opcode::Add,
-            2 => Opcode::Mult,
-            3 => Opcode::Input,
-            4 => Opcode::Output,
-            99 => Opcode::Stop,
-            _ => Opcode::Err,
-        }
-    }
-}
+// impl IntCodeProgram {
+//     pub fn current_instruction(&self) -> String {
+//         let x = String::from(format!("{:?}", self.parse_current_instruction()));
+//         x
+//     }
 
-impl Opcode {
-    pub fn len(&self) -> usize {
-        match self {
-            Opcode::Add => 4,
-            Opcode::Mult => 4,
-            Opcode::Input => 2,
-            Opcode::Output => 2,
-            Opcode::Stop => 1,
-            Opcode::Err => 0,
-        }
-    }
-}
+//     pub fn exec_step(&mut self) -> Option<()> {
+//         // let inst = &self.instructions[self.pc];
+//         let inst = self.parse_current_instruction().unwrap();
+//         let mut ret = match inst.opcode {
+//             Opcode::Add => {
+//                 self.state[inst.out] = self.state[inst.in1] + self.state[inst.in2];
+//                 Some(())
+//             }
+//             Opcode::Mult => {
+//                 self.state[inst.out] = self.state[inst.in1] * self.state[inst.in2];
+//                 Some(())
+//             }
+//             Opcode::Input => None,
+//             Opcode::Output => None,
+//             Opcode::Stop => None,
+//             Opcode::Err => {
+//                 println!("Illegal Opcode {}", self.state[self.pc]);
+//                 None
+//             }
+//         };
+//         self.pc += 4;
+//         if self.pc >= self.state.len() {
+//             self.pc = 0;
+//             ret = None;
+//         }
+//         ret
+//     }
 
-pub struct IntCodeProgram {
-    initial_state: Vec<i32>,
-    state: Vec<i32>,
-    pc: usize,
-}
+//     pub fn run(&mut self) -> i32 {
+//         while self.exec_step() != None {}
+//         self.state[0]
+//     }
 
-impl IntCodeProgram {
-    pub fn current_instruction(&self) -> String {
-        let x = String::from(format!("{:?}", self.parse_current_instruction()));
-        x
-    }
+//     pub fn run_with(&mut self, noun: i32, verb: i32) -> i32 {
+//         self.state[1] = noun;
+//         self.state[2] = verb;
+//         self.pc = 0;
+//         self.run()
+//     }
 
-    pub fn exec_step(&mut self) -> Option<()> {
-        // let inst = &self.instructions[self.pc];
-        let inst = self.parse_current_instruction().unwrap();
-        let mut ret = match inst.opcode {
-            Opcode::Add => {
-                self.state[inst.out] = self.state[inst.in1] + self.state[inst.in2];
-                Some(())
-            }
-            Opcode::Mult => {
-                self.state[inst.out] = self.state[inst.in1] * self.state[inst.in2];
-                Some(())
-            }
-            Opcode::Input => None,
-            Opcode::Output => None,
-            Opcode::Stop => None,
-            Opcode::Err => {
-                println!("Illegal Opcode {}", self.state[self.pc]);
-                None
-            }
-        };
-        self.pc += 4;
-        if self.pc >= self.state.len() {
-            self.pc = 0;
-            ret = None;
-        }
-        ret
-    }
+//     pub fn reset(&mut self) {
+//         self.state = self.initial_state.clone();
+//         self.pc = 0;
+//     }
 
-    pub fn run(&mut self) -> i32 {
-        while self.exec_step() != None {}
-        self.state[0]
-    }
+//     fn parse_current_instruction(&self) -> Option<IntCode> {
+//         let op: Opcode = self.state[self.pc].into();
 
-    pub fn run_with(&mut self, noun: i32, verb: i32) -> i32 {
-        self.state[1] = noun;
-        self.state[2] = verb;
-        self.pc = 0;
-        self.run()
-    }
+//         let slice = &self.state[self.pc..self.pc + op.len()];
 
-    pub fn reset(&mut self) {
-        self.state = self.initial_state.clone();
-        self.pc = 0;
-    }
+//         // IntCode::from(slice);
+//         match IntCode::try_from(slice) {
+//             Ok(x) => Some(x),
+//             Err(_) => None,
+//         }
+//     }
 
-    fn parse_current_instruction(&self) -> Option<IntCode> {
-        let op: Opcode = self.state[self.pc].into();
-
-        let slice = &self.state[self.pc..self.pc + op.len()];
-
-        // IntCode::from(slice);
-        match IntCode::try_from(slice) {
-            Ok(x) => Some(x),
-            Err(_) => None,
-        }
-    }
-
-    pub fn new(data: &Vec<i32>) -> Self {
-        // let test = data.iter();
-        // let state: Vec<i32> = data.iter().map(|x| (*x).from()).collect();
-        IntCodeProgram {
-            initial_state: data.clone(),
-            state: data.clone(),
-            // instructions: IntCodeProgram::parse_instructions(data).unwrap(),
-            pc: 0,
-        }
-    }
-}
-
-impl TryFrom<&str> for IntCode {
-    type Error = &'static str;
-    fn try_from(text: &str) -> Result<Self, Self::Error> {
-        let data = parse_program(text);
-
-        Err("Fuck")
-    }
-}
+//     pub fn new(data: &Vec<i32>) -> Self {
+//         // let test = data.iter();
+//         // let state: Vec<i32> = data.iter().map(|x| (*x).from()).collect();
+//         IntCodeProgram {
+//             initial_state: data.clone(),
+//             state: data.clone(),
+//             // instructions: IntCodeProgram::parse_instructions(data).unwrap(),
+//             pc: 0,
+//         }
+//     }
+// }
 
 mod int_code_computer {
+    use std::convert::TryFrom;
+
+    pub fn parse_program(input: &str) -> Result<Vec<i32>, std::num::ParseIntError> {
+        input.split(',').map(|s| s.parse()).collect()
+    }
+
+    impl IntComputer {
+        // type Error = &'static str;
+        pub fn new(data: &Vec<i32>) -> Self {
+            IntComputer {
+                initial_state: data.clone(),
+                state: data.clone(),
+                pc: 0,
+                in1: 0,
+                in2: 0,
+                out: 0,
+                op: Opcode::Err,
+            }
+        }
+
+        fn load_instruction_at_pc(&mut self) -> Result<(Opcode), String> {
+            match self.state.get(self.pc) {
+                Some(inst) => {
+                    let inst = *inst;
+                    Ok((inst.into()))
+                }
+                None => {
+                    return Err(format!(
+                        "PC out of bounds. PC: {} buffer {}",
+                        self.pc,
+                        self.state.len()
+                    ))
+                }
+            }
+        }
+
+        fn load_param(&self, value: isize, param: Param) -> Result<i32, String> {
+            match param {
+                Param::Positional => match self.state.get(value as usize) {
+                    Some(&p) => Ok(p),
+                    None => Err(format!("Index {} out of Bounds", value)),
+                },
+                Param::Immediate => Ok(value as i32),
+                Param::Invalid => Err(format!("Illegal param, value {}", value)),
+            }
+        }
+
+        fn store_param(&mut self, value: i32, location: isize) -> Result<(), String> {
+            match self.state.iter_mut().skip(location as usize).next() {
+                Some(x) => {
+                    *x = value;
+                    Ok(())
+                }
+                None => Err(format!(
+                    "Failed to store value@location: {}@{}",
+                    value, location
+                )),
+            }
+        }
+
+        fn execute_loaded_instruction(&mut self) -> Result<Opcode, String> {
+            self.op = match self.load_instruction_at_pc() {
+                Ok(op) => op,
+                Err(e) => return Err(e),
+            };
+            let result = match self.op {
+                Opcode::Add(in1, in2, _out) => {
+                    let mut iter = self.state.iter().skip(self.pc).take(3);
+                    self.in1 = self.load_param(*(iter.next().unwrap()) as isize, in1)?;
+                    self.in2 = self.load_param(*(iter.next().unwrap()) as isize, in2)?;
+                    Ok(())
+                }
+                Opcode::Mult(in1, in2, _out) => {
+                    let mut iter = self.state.iter().skip(self.pc).take(3);
+                    self.in1 = self.load_param(*(iter.next().unwrap()) as isize, in1)?;
+                    self.in2 = self.load_param(*(iter.next().unwrap()) as isize, in2)?;
+                    Ok(())
+                }
+                Opcode::Input => Ok(()), /* Prompt to input by calling an input function */
+                Opcode::Output => {
+                    let index = match self.state.get(self.pc + 1) {
+                        Some(&x) => x,
+                        None => return Err(format!("Output index {} out of bounds", self.pc + 1)),
+                    };
+                    println!(
+                        "Value at {}: {}",
+                        self.pc + 1,
+                        self.load_param(index as isize, Param::Immediate)?
+                    );
+                    Ok(())
+                } /* Output to the console */
+                Opcode::Err => {
+                    let msg = format!("Invalid instruction @ {}", self.pc);
+                    println!("{}", msg);
+                    Err(msg)
+                } /* failed to parse instruction, crash execution */
+                Opcode::Stop => Ok(()),  /* done executing */
+            };
+
+            match result {
+                Ok(()) => Ok(self.op.clone()),
+                Err(x) => Err(x),
+            }
+            // Err(format!("Fuck"))
+        }
+
+        // fn execute_instruction(&self, op: Opcode) -> Result<(), String> {
+        //     // let inst = &self.instructions[self.pc];
+        //     let mut ret = match op.opcode {
+        //         Opcode::Add => {
+        //             self.state[inst.out] = self.state[inst.in1] + self.state[inst.in2];
+        //             Some(())
+        //         }
+        //         Opcode::Mult => {
+        //             self.state[inst.out] = self.state[inst.in1] * self.state[inst.in2];
+        //             Some(())
+        //         }
+        //         Opcode::Input => None,
+        //         Opcode::Output => None,
+        //         Opcode::Stop => None,
+        //         Opcode::Err => {
+        //             println!("Illegal Opcode {}", self.state[self.pc]);
+        //             None
+        //         }
+        //     };
+        //     self.pc += 4;
+        //     if self.pc >= self.state.len() {
+        //         self.pc = 0;
+        //         ret = None;
+        //     }
+        //     ret
+        //     // Err(format!("Fuck"))
+        // }
+    }
+    // opcode = (*data.first().unwrap()).into()
+
+    impl TryFrom<&str> for IntComputer {
+        type Error = &'static str;
+        fn try_from(text: &str) -> Result<Self, Self::Error> {
+            match parse_program(text) {
+                Ok(program) => Ok(IntComputer::new(&program)),
+                Err(_) => Err("Failed to parse IntCode Program"),
+            }
+        }
+    }
+
     #[derive(Debug, Clone, Copy)]
     pub enum Param {
         Positional,
@@ -166,21 +290,21 @@ mod int_code_computer {
         Invalid,
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub enum Opcode {
-        Add { in1: Param, in2: Param, out: Param },
-        Mult { in1: Param, in2: Param, out: Param },
+        Add(Param, Param, Param),
+        Mult(Param, Param, Param),
         Stop,
-        Input(Param),
-        Output(Param),
+        Input,
+        Output,
         Err,
     }
     trait ParseParam {
         fn parse_params(input: u32);
     }
 
-    impl From<u32> for Opcode {
-        fn from(x: u32) -> Self {
+    impl From<i32> for Opcode {
+        fn from(x: i32) -> Self {
             let op = if x > 99 { x % 100 } else { x };
             let param = x / 100;
 
@@ -198,18 +322,18 @@ mod int_code_computer {
             let mut x = result.iter();
 
             match op {
-                1 => Opcode::Add {
-                    in1: *x.next().unwrap(),
-                    in2: *x.next().unwrap(),
-                    out: *x.next().unwrap(),
-                },
-                2 => Opcode::Mult {
-                    in1: *x.next().unwrap(),
-                    in2: *x.next().unwrap(),
-                    out: *x.next().unwrap(),
-                },
-                3 => Opcode::Input(*x.next().unwrap()),
-                4 => Opcode::Output(*x.next().unwrap()),
+                1 => Opcode::Add(
+                    /* in1: */ *x.next().unwrap(),
+                    /* in2: */ *x.next().unwrap(),
+                    /* out: */ *x.next().unwrap(),
+                ),
+                2 => Opcode::Mult(
+                    /* in1: */ *x.next().unwrap(),
+                    /* in2: */ *x.next().unwrap(),
+                    /* out: */ *x.next().unwrap(),
+                ),
+                3 => Opcode::Input,
+                4 => Opcode::Output,
                 99 => Opcode::Stop,
                 _ => Opcode::Err,
             }
@@ -218,18 +342,10 @@ mod int_code_computer {
     impl Opcode {
         pub fn len(&self) -> usize {
             match self {
-                Opcode::Add {
-                    in1: _,
-                    in2: _,
-                    out: _,
-                } => 4,
-                Opcode::Mult {
-                    in1: _,
-                    in2: _,
-                    out: _,
-                } => 4,
-                Opcode::Input(_) => 2,
-                Opcode::Output(_) => 2,
+                Opcode::Add(_, _, _) => 4,
+                Opcode::Mult(_, _, _) => 4,
+                Opcode::Input => 2,
+                Opcode::Output => 2,
                 Opcode::Stop => 1,
                 Opcode::Err => 0,
             }
@@ -240,22 +356,32 @@ mod int_code_computer {
         initial_state: Vec<i32>,
         state: Vec<i32>,
         pc: usize,
-        in1: isize,
-        in2: isize,
-        out: isize,
+        in1: i32,
+        in2: i32,
+        out: i32,
         op: Opcode,
     }
 }
 
 mod tests {
-    use super::*;
+    use super::int_code_computer::*;
 
     #[test]
-    fn test_new() {
+    fn test_string_parsing_ok() {
         let correct = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
         let input = String::from("1,9,10,3,2,3,11,0,99,30,40,50");
         let result = parse_program(&input);
         assert_eq!(true, result.is_ok());
         assert_eq!(correct, result.unwrap());
     }
+
+    #[test]
+    fn test_string_parsing_error() {
+        let input = String::from("1,9,10,3,2,3,a11,0,99,30,40,50");
+        let result = parse_program(&input);
+        assert_eq!(true, result.is_err());
+    }
+
+    #[test]
+    fn test_opcode_add() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,21 @@ fn main() {
         buf.push_str(&line);
     }
     // let mut buf = String::from("1,9,10,3,2,3,11,0,99,30,40,50");
-    let program_data = parse_program(&buf).unwrap();
+    // let program_data = parse_program(&buf).unwrap();
 
-    let mut ga = IntCodeProgram::new(&program_data);
-    // println!{"The Result is {}", ga.run()};
+    // let mut ga = IntCodeProgram::new(&program_data);
+    // // println!{"The Result is {}", ga.run()};
 
-    'outer: for noun in 0..99 {
-        for verb in 0..99 {
-            if ga.run_with(noun, verb) == 19690720 {
-                println!("Found solution at {} {}", noun, verb);
-                println!("Answer is {}", 100 * noun + verb);
-                break 'outer;
-            }
-            ga.reset();
-        }
-    }
+    // 'outer: for noun in 0..99 {
+    //     for verb in 0..99 {
+    //         if ga.run_with(noun, verb) == 19690720 {
+    //             println!("Found solution at {} {}", noun, verb);
+    //             println!("Answer is {}", 100 * noun + verb);
+    //             break 'outer;
+    //         }
+    //         ga.reset();
+    //     }
+    // }
 }
 
 mod tests {
