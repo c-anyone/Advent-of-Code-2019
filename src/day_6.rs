52368539e710681337585f0cf32fd9e07a42e127
Fabio Pungg <fabiopungg@gmail.com>
2019-12-30 18:08:02 +0100

    Still working on the tree, compiles...

--- src/day_6.rs
+++ src/day_6.rs
@@ -57,7 +57,9 @@ What is the total number of direct and indirect orbits in your map data?
 */
 
 use itertools::Itertools;
+use std::cell::{RefCell, RefMut};
 use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -68,13 +70,90 @@ pub fn run() {
     println!("Result is {}", result);
 }
 
-pub fn get_gravity_map_sum(filename: &str) -> u32 {
+#[derive(Debug, Clone)]
+struct Node {
+    id: String,
+    head: String,
+    children: Option<RefCell<Vec<Node>>>,
+}
+
+#[derive(Debug, Clone)]
+struct Tree {
+    nodes: HashMap<String, Node>,
+    head: Option<Box<Node>>,
+}
+
+impl Node {
+    fn add_children(&mut self, node: Node) {
+        if self.children.is_none() {
+            self.children = Some(RefCell::new(Vec::new()));
+        }
+        let mut x: RefMut<_> = self.children.as_ref().unwrap().borrow_mut();
+        x.push(node);
+    }
+
+    fn new(id: &str, head: &str, children: Option<RefCell<Vec<Node>>>) -> Self {
+        Node {
+            id: id.to_string(),
+            head: head.to_string(),
+            children: children,
+        }
+    }
+}
+
+impl TryFrom<&str> for Node {
+    type Error = &'static str;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let (head, id) = parse_line(value).ok_or("error parsing line")?;
+        Ok(Node {
+            id: id,
+            head: head,
+            children: None,
+        })
+    }
+}
+
+impl Tree {
+    fn new() -> Self {
+        Tree {
+            nodes: HashMap::new(),
+            head: None,
+        }
+    }
+
+    fn insert(&mut self, node: Node) -> Option<()> {
+        if !self.nodes.contains_key(&node.head) {
+            let head_node = Node::new(&node.id, "", None);
+            self.nodes.insert(head_node.id.clone(), head_node);
+        }
+        if self.nodes.contains_key(&node.id) {
+            self.nodes.insert(node.id.clone(), node.to_owned());
+        }
+
+        Some(())
+    }
+}
+
+fn construct_tree(filename: &str) {
     let reader = BufReader::new(File::open(filename).expect("File not found"));
 
+    let mut tree = Tree::new();
+
+    reader.lines().enumerate().for_each(|(n, x)| {
+        let x = x.unwrap();
+        let mut node = Node::try_from(x.as_ref()).unwrap();
+        tree.insert(node);
+    });
+    // for line in reader.lines()
+}
+
+pub fn get_gravity_map_sum(filename: &str) -> u32 {
+    let reader = BufReader::new(File::open(filename).expect("File not found"));
     let iter: Vec<(String, String)> = reader
         .lines()
         .filter_map(|x| parse_line(&x.unwrap()).to_owned())
         .collect();
+
     let mut hm: HashMap<String, u32> = HashMap::new();
     for (grav, id) in iter {
         // let val = hm.get_mut()
@@ -87,7 +166,7 @@ pub fn get_gravity_map_sum(filename: &str) -> u32 {
     hm.values().sum()
 }
 
-pub fn parse_line(line: &str) -> Option<(String, String)> /* -> (&str, &str) */ {
+fn parse_line(line: &str) -> Option<(String, String)> /* -> (&str, &str) */ {
     match line.splitn(2, ')').collect_tuple() {
         Some((x, y)) => Some((x.to_string(), y.to_string())),
         None => return None,
@@ -98,8 +177,14 @@ pub fn parse_line(line: &str) -> Option<(String, String)> /* -> (&str, &str) */ 
 mod test {
     use super::*;
     #[test]
-    fn test_sum() {
+    fn test_sum_testinput() {
         let result = get_gravity_map_sum("input_day6_test.txt");
         assert_eq!(result, 42);
+    }
+
+    #[test]
+    fn test_sum_puzzle() {
+        let result = get_gravity_map_sum("input_day6.txt");
+        assert_eq!(result > 2190, true);
     }
 }
